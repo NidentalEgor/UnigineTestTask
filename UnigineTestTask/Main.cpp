293bc28e227f62d8802e4b612d89b66c630cc044
Egor <nidentalegor@gmail.com>
2017-06-23 06:07:19 +0700

    2.

--- UnigineTestTask/Main.cpp
+++ UnigineTestTask/Main.cpp
@@ -6,8 +6,72 @@
 
 class UrlStatisticsCollector
 {
+private:
+
+	class SymbolChecker
+	{
+	public:
+		enum class CorrectSymbolsSetType
+		{
+			Domen,
+			Path
+		};
+
+		SymbolChecker(
+			const CorrectSymbolsSetType correct_symbols_set_type)
+			: correct_symbols_set_type_(correct_symbols_set_type)
+		{
+		};
+
+		bool CheckSymbol(
+			const char symbol) const
+		{
+			if (correct_symbols_set_type_ == CorrectSymbolsSetType::Domen)
+			{
+				return CheckDomen(symbol);
+			}
+			else
+			{
+				return CheckPath(symbol);
+			}
+		}
+
+	private:
+		bool CheckLettersAndNumbers(
+			const char symbol) const
+		{
+			return (symbol >= 'a' && symbol <= 'z') ||
+				(symbol >= 'A' && symbol <= 'Z') ||
+				(symbol >= '0' && symbol <= '9');
+		}
+
+		bool CheckDomen(
+			const char symbol) const
+		{
+			return CheckLettersAndNumbers(symbol) ||
+				symbol == '.' ||
+				symbol == '-';
+		}
+
+		bool CheckPath(
+			const char symbol) const
+		{
+			return CheckLettersAndNumbers(symbol) ||
+				symbol == '.' ||
+				symbol == ',' ||
+				symbol == '/' ||
+				symbol == '+' ||
+				symbol == '_';
+		}
+
+	private:
+		CorrectSymbolsSetType correct_symbols_set_type_;
+	};
+
 public:
 	UrlStatisticsCollector()
+		: domen_symbol_checker_(SymbolChecker::CorrectSymbolsSetType::Domen)
+		, path_symbol_checker_(SymbolChecker::CorrectSymbolsSetType::Path)
 	{	
 	}
 
@@ -35,91 +99,59 @@ public:
 		}
 	}
 
-private:
+
 
 	std::string::size_type IsPrefixCorrect(
 			const std::string& line,
 			std::string::size_type position)
 	{
-		if (AreSubstringsCoincide(line, position, "://", 0, 3))
+		if (AreSubstringsCoincide(line, position + 4, "://", 0, 3)) // часть "http" уже проверена
 		{
-			return position + 3;
+			return position + 7;
 		}
-		if (AreSubstringsCoincide(line, position, "s://", 0, 4))
+		if (AreSubstringsCoincide(line, position + 4, "s://", 0, 4)) // часть "http" уже проверена
 		{
-			return position + 4;
+			return position + 8;
 		}
 
 		return std::string::npos;
 	}
 
-	class SymbolChecker
+	std::string::size_type GetPath(
+			const std::string& line,
+			const std::string::size_type position) const
 	{
-	public:
-
-		enum class CorrectSymbolsSetType
+		for (size_t i = position; i < line.size(); ++i)
 		{
-			Domen,
-			Path
-		};
-
-		SymbolChecker() {};
-
-		bool CheckSymbol(
-				const CorrectSymbolsSetType correct_symbols_set_type,
-				const char symbol)
-		{
-			if (correct_symbols_set_type == CorrectSymbolsSetType::Domen)
+			if (!path_symbol_checker_.CheckSymbol(line[i]))
 			{
-				return CheckDomen(symbol);
-			}
-			else
-			{
-				return CheckPath(symbol);
+				return i;
 			}
 		}
 
-	private:
-		bool CheckLettersAndNumbers(
-				const char symbol)
-		{
-			return (symbol >= 'a' && symbol <= 'z') ||
-					(symbol >= 'A' && symbol <= 'Z') ||
-					(symbol >= '0' && symbol <= '9');
-		}
-
-		bool CheckDomen(const char symbol)
-		{
-			return CheckLettersAndNumbers(symbol) ||
-					symbol == '.' ||
-					symbol == '-';
-		}
-
-		bool CheckPath(const char symbol)
-		{
-			return CheckLettersAndNumbers(symbol) ||
-					symbol == '.' ||
-					symbol == ',' ||
-					symbol == '/' ||
-					symbol == '+' ||
-					symbol == '_';
-		}
-	};
-
-	
-
-	bool IsCorrectSymbol(
-			const SymbolChecker::CorrectSymbolsSetType correct_symbols_set_type)
-	{
-		std::string correct_symbols;
-		
+		return std::string::npos;
 	}
 
-	std::string GetDomen(
+	std::string::size_type GetDomen(
 			const std::string& line,
-			const std::string::size_type position)
+			const std::string::size_type position) const
 	{
-		
+		size_t i = 0;
+		for (i = position
+			; i < line.size()
+			; ++i)
+		{
+			if (line[i] == '/' || line[i] == ' ')
+			{
+				return i;
+			}
+			if (!domen_symbol_checker_.CheckSymbol(line[i]))
+			{
+				return std::string::npos;
+			}
+		}
+
+		return i;
 	}
 
 	std::string::size_type ParseUrl(
@@ -128,14 +160,53 @@ private:
 	{
 		const std::string::size_type after_prefix_position =
 				IsPrefixCorrect(line, position);
+		std::string::size_type after_path_position = 0;
 		if (after_prefix_position != std::string::npos)
 		{
+			const std::string::size_type after_domen_position =
+					GetDomen(line, after_prefix_position);
+			if (after_domen_position != std::string::npos)
+			{
+				// Добавляем в хеш\мапу
+				std::string domen(
+						line.begin() + after_prefix_position,
+						line.begin() + after_domen_position);
+				std::cout << "domen = " << domen << std::endl;
+			}
+			else
+			{
+				return std::string::npos;
+			}
+
+			if (line[after_domen_position] != ' ')
+			{
+				after_path_position =
+					GetPath(line, after_domen_position);
+				if (after_path_position != std::string::npos)
+				{
+					// Добавляем в хеш\мапу
+					std::string path(
+						line.begin() + after_domen_position,
+						line.begin() + after_path_position);
+					std::cout << "path = " << path << std::endl;
+				}
+				else
+				{
+					return std::string::npos;
+				}
+			}
+			else
+			{
+				std::cout << "path = '/'" << std::endl;
+				// добавляем '/'  так как путь пустой
+			}
 			
 		}
 		else
 		{
 			return position + 4;
 		}
+		return after_path_position;
 	}
 
 	bool AreSubstringsCoincide(
@@ -222,7 +293,8 @@ private:
 	}
 
 private:
-
+	SymbolChecker domen_symbol_checker_;
+	SymbolChecker path_symbol_checker_;
 };
 
 int main ()
