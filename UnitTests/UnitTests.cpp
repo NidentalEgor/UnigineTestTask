1fc441f5553700eac23d3c7038ebbc88a4f6b0e9
Egor Nidental <nidentalegor@gmail.com>
2017-11-24 01:16:46 +0700

    Unit tests refactoring.

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,99 +1,120 @@
-﻿#include "stdafx.h"
-#include "CppUnitTest.h"
-#include "../UnigineTestTask/Main.cpp"
+﻿#include <iterator>
+#include <string>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+#include "gtest/gtest.h"
 
-namespace UnitTests
-{		
-	TEST_CLASS(UnitTest1)
-	{
-	private:
-		const std::string test_data_path_common_prefix_ = R"(D:/Projects/Repositories/UnigineTestTask/UnitTests/TestData/)";
+#include "../UrlStatisticsCollector/UrlStatisticsCollector.cpp"
 
-		bool AreFilesEqual(
-				const std::string& first_file_path, 
-				const std::string& second_file_path) const
-		{
-			std::ifstream first_file(first_file_path);
-			std::ifstream second_file(second_file_path);
+class SomeName
+        : public testing::Test
+{
+protected:
+    bool AreFilesEqual(
+            const std::string& first_file_path, 
+            const std::string& second_file_path) const
+    {
+        std::ifstream first_file(
+                first_file_path, std::ios::in | std::ios::binary);
 
-			const std::istream_iterator<char> first_file_begin(first_file), first_file_end;
-			const std::istream_iterator<char> second_file_begin(second_file);
+        if (!first_file)
+        {
+            return false;
+        }
 
-			return std::equal(first_file_begin, first_file_end, second_file_begin);
-		}
+        std::ifstream second_file(
+                second_file_path, std::ios::in | std::ios::binary);
 
-	public:
-		
-		TEST_METHOD(EmptyFileTest)
-		{
-			const std::string input_file_path =
-					test_data_path_common_prefix_ + "EmptyFileTest/Input.txt";
-			const std::string output_file_path =
-					test_data_path_common_prefix_ + "EmptyFileTest/Output.txt";
-			const std::string expected_result_file_path =
-					test_data_path_common_prefix_ + "EmptyFileTest/ExpectedResult.txt";
+        if (!second_file)
+        {
+            return false;
+        }
 
-			UrlStatisticsCollector url_statistics_collector(
-					input_file_path);
-			url_statistics_collector.WriteStatistics(
-					output_file_path,
-					5);
+        return std::string(
+                       std::istreambuf_iterator<char>(first_file),
+                       std::istreambuf_iterator<char>()) ==
+                std::string(
+                       std::istreambuf_iterator<char>(second_file),
+                       std::istreambuf_iterator<char>());
+    }
+    
+protected:
+    const std::string test_data_path_common_prefix_ = R"(../../../../UnitTests/TestData/)";
+};
 
-			Assert::IsTrue(
-					AreFilesEqual(
-						output_file_path,
-						expected_result_file_path),
-					L"Error!",
-					LINE_INFO());
-		}
+TEST_F(SomeName, EmptyFileTest)
+{
+    const std::string input_file_path =
+            test_data_path_common_prefix_ + "EmptyFileTest/Input.txt";
+    const std::string output_file_path =
+            test_data_path_common_prefix_ + "EmptyFileTest/Output.txt";
+    const std::string expected_result_file_path =
+            test_data_path_common_prefix_ + "EmptyFileTest/ExpectedResult.txt";
+ 
+    UrlStatisticsCollector url_statistics_collector(
+            input_file_path);
+    url_statistics_collector.WriteStatistics(
+            output_file_path,
+            5);
 
-		TEST_METHOD(OneUrlTest)
-		{
-			const std::string input_file_path =
-				test_data_path_common_prefix_ + "OneUrlTest/Input.txt";
-			const std::string output_file_path =
-				test_data_path_common_prefix_ + "OneUrlTest/Output.txt";
-			const std::string expected_result_file_path =
-				test_data_path_common_prefix_ + "OneUrlTest/ExpectedResult.txt";
+    ASSERT_TRUE(AreFilesEqual(output_file_path, expected_result_file_path));
+}
 
-			UrlStatisticsCollector url_statistics_collector(
-				input_file_path);
-			url_statistics_collector.WriteStatistics(
-				output_file_path,
-				5);
+TEST_F(SomeName, OneUrlTest)
+{
+    const std::string input_file_path =
+            test_data_path_common_prefix_ + "OneUrlTest/Input.txt";
+    const std::string output_file_path =
+            test_data_path_common_prefix_ + "OneUrlTest/Output.txt";
+    const std::string expected_result_file_path =
+            test_data_path_common_prefix_ + "OneUrlTest/ExpectedResult.txt";
 
-			Assert::IsTrue(
-				AreFilesEqual(
-					output_file_path,
-					expected_result_file_path),
-				L"Error!",
-				LINE_INFO());
-		}
+    UrlStatisticsCollector url_statistics_collector(
+            input_file_path);
+    url_statistics_collector.WriteStatistics(
+            output_file_path,
+            5);
 
-		TEST_METHOD(AllCases)
-		{
-			const std::string input_file_path =
-				test_data_path_common_prefix_ + "AllCases/Input.txt";
-			const std::string output_file_path =
-				test_data_path_common_prefix_ + "AllCases/Output.txt";
-			const std::string expected_result_file_path =
-				test_data_path_common_prefix_ + "AllCases/ExpectedResult.txt";
+    ASSERT_TRUE(AreFilesEqual(output_file_path, expected_result_file_path));    
+}
 
-			UrlStatisticsCollector url_statistics_collector(
-				input_file_path);
-			url_statistics_collector.WriteStatistics(
-				output_file_path,
-				5);
+TEST_F(SomeName, AllCases)
+{
+    const std::string input_file_path =
+            test_data_path_common_prefix_ + "AllCases/Input.txt";
+    const std::string output_file_path =
+            test_data_path_common_prefix_ + "AllCases/Output.txt";
+    const std::string expected_result_file_path =
+            test_data_path_common_prefix_ + "AllCases/ExpectedResult.txt";
 
-			Assert::IsTrue(
-				AreFilesEqual(
-					output_file_path,
-					expected_result_file_path),
-				L"Error!",
-				LINE_INFO());
-		}
+    UrlStatisticsCollector url_statistics_collector(
+            input_file_path);
+    url_statistics_collector.WriteStatistics(
+            output_file_path,
+            5);
 
-	};
+    ASSERT_TRUE(AreFilesEqual(output_file_path, expected_result_file_path));    
+}
+
+TEST_F(SomeName, BigTestFromUnigine)
+{
+    const std::string input_file_path =
+            test_data_path_common_prefix_ + "BigTestFromUnigine/Input.txt";
+    const std::string output_file_path =
+            test_data_path_common_prefix_ + "BigTestFromUnigine/Output.txt";
+    const std::string expected_result_file_path =
+            test_data_path_common_prefix_ + "BigTestFromUnigine/ExpectedResult.txt";
+
+    UrlStatisticsCollector url_statistics_collector(
+            input_file_path);
+    url_statistics_collector.WriteStatistics(
+            output_file_path,
+            100);
+
+    ASSERT_TRUE(AreFilesEqual(output_file_path, expected_result_file_path));    
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
